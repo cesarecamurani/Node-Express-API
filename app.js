2402be9aa3350cd065def3bea320a00e50d6ff39
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-23 14:49:15 +0000

    Post method for adding todos added to app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './db/db';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
@@ -9,6 +13,31 @@ app.get('/api/v1/todos', (req, res) => {
     message: 'todos retrieved successfully',
     todos: db
   })
+});
+
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+ const todo = {
+   id: db.length + 1,
+   title: req.body.title,
+   description: req.body.description
+ }
+ db.push(todo);
+ return res.status(201).send({
+   success: 'true',
+   message: 'todo added successfully',
+   todo
+ })
 });
 
 const PORT = 5000;
